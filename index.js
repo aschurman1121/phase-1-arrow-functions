18cccfc1b61c013f5f59b00ab8764f79005cf9d4
alexandra <alexandraschurman@gmail.com>
2022-01-21 08:02:44 -0800

    commit tree f1b73d65a06b1218cb7cf99dc1f251e4227c3826

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+// const add = (num1, num2) => num1 + num2;
+
+// no braces means an automatic 'return'
+//with one parameter the () becomes optional
+
+
+const divide = (num1, num2) => num1 / num2
+
+divide( num2, num1)
+
+let num1 = 2000
+let num2 = 100
+
+// const square = (x) => x **
+
+const add = (a, b) => a + b
+let a = 3
+let b = 4
+
